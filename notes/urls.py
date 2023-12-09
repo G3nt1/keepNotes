363e6a168a890172cb5b30a60aca5ead41bb9b04
G3nt1 <mateligentjan@gmail.com>
2023-12-09 18:17:41 +0100

    Create, edit and delete Notes

--- notes/urls.py
+++ notes/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('edit-user/<int:user_id>', views.edit_user, name='edit_user'),
     path('logout/', views.logout_user, name='logout'),
+
+    path('create-notes', views.create_notes, name='create_notes'),
+    path('edit-note/<int:notes_id>', views.edit_notes, name='edit_notes'),
+    path('delete_notes/<int:notes_id>', views.delete_notes, name='delete_notes'),
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -4,23 +4,60 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from notes.forms import CreateUserForm, LoginUserForm, CreateNotes
 from notes.models import BigNotes
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# Notes
 
 
 def home(request):
     notes = BigNotes.objects.all().order_by('-created_at')
+    notes_per_page = 7
+    paginator = Paginator(notes, notes_per_page)
+    page = request.GET.get('page')
 
-    return render(request, 'home.html', {'notes': notes})
+    try:
+        current_notes = paginator.page(page)
+    except PageNotAnInteger:
+        current_notes = paginator.page(1)
+    except EmptyPage:
+        current_notes = paginator.page(paginator.num_pages)
+
+    return render(request, 'home.html', {'notes': current_notes})
 
 
- # if request.method == "POST":
- #        form = CreateNotes(request.POST)
- #        if form.is_valid():
- #            form.save()
- #            return redirect('home')
- #    else:
- #        form = CreateNotes()
+def create_notes(request):
+    if request.method == "POST":
+        form = CreateNotes(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CreateNotes()
+    return render(request, 'create_notes.html', {'form': form})
 
 
+def edit_notes(request, notes_id):
+    notes = get_object_or_404(BigNotes, id=notes_id)
+    if request.method == 'POST':
+        form = CreateNotes(request.POST, instance=notes)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CreateNotes(instance=notes)
+    return render(request, 'edit_notes.html', {'form': form})
+
+
+def delete_notes(request, notes_id):
+    notes = get_object_or_404(BigNotes, id=notes_id)
+    if request.method == 'POST':
+        notes.delete()
+        return redirect('home')
+    return render(request, 'delete_notes.html', {'notes': notes})
+
+
+# Users
 def register_user(request):
     if request.method == "POST":
         form = CreateUserForm(request.POST)
